c40d30255d55f02685e40cb319059f91ec8539c5
Adriano Reis <adriano_silvareis@hotmail.com>
2024-05-01 01:15:37 -0300

    feat: configure cors

--- src/express.config.ts
+++ src/express.config.ts
@@ -1,7 +1,9 @@
-import "./logger-config";
-
+import cors from "cors";
 import express from "express";
+import "./logger-config";
 
 export const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
