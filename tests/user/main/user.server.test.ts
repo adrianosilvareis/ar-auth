d6d6b356058b51543222ddae135f7985c5e017f0
Adriano Reis <adriano_silvareis@hotmail.com>
2024-05-06 14:26:45 -0300

    tests: create refresh token tests

--- tests/user/main/user.server.test.ts
+++ tests/user/main/user.server.test.ts
@@ -1,11 +1,18 @@
 import { diContainer } from "@/containers";
+import "@/sessions/main/container";
+
 import "@/logger-config";
 
 import { Cache } from "@/cache/cache";
 import { app } from "@/express.config";
 import { Logger } from "@/logger/logger";
+import { right } from "@/protocols/either/either";
+import { SessionApplication } from "@/sessions/applications/session.application";
+import { SessionDatabase } from "@/sessions/applications/session.database";
+import { SessionMockedDatabase } from "@/sessions/infrastructure/gateways/databases/sessions-mocked.database";
 import { UserApplication } from "@/user/applications/user.application";
 import { UserDatabase } from "@/user/applications/user.database";
+import { UserToken } from "@/user/applications/user.token";
 import { UserMockedDatabase } from "@/user/infrastructure/gateways/databases/users-mocked.database";
 import { appUser } from "@/user/main/user.server";
 import { StatusCodes } from "http-status-codes";
@@ -16,6 +23,9 @@ Logger.pause();
 appUser(app);
 
 const database = diContainer.get(UserDatabase) as UserMockedDatabase;
+const sessionDatabase = diContainer.get(
+  SessionDatabase
+) as SessionMockedDatabase;
 const cache = diContainer.get(Cache);
 
 const supertest = request(app);
@@ -28,6 +38,7 @@ describe("User Controller", () => {
   beforeEach(() => {
     cache.clear();
     database.users = [];
+    sessionDatabase.sessions = [];
   });
 
   describe("POST /user/register", () => {
@@ -151,6 +162,53 @@ describe("User Controller", () => {
 
       expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
       expect(response.body).toEqual("User Unauthorized");
+    });
+  });
+
+  describe("GET /user/refresh-token", () => {
+    it("should return status 401 when authorization token is not provided", async () => {
+      const response = await supertest.get("/refresh-token");
+
+      expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+      expect(response.body).toBe("Unauthorized");
+    });
+
+    it("should return status 401 when authorization token is invalid", async () => {
+      const token = "invalid token";
+      const response = await supertest
+        .get("/refresh-token")
+        .set("Authorization", `bearer ${token}`);
+
+      expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+      expect(response.body).toEqual({ name: "InvalidTokenError" });
+    });
+
+    it("should return status 401 when authorization token is invalid", async () => {
+      const managerToken = diContainer.get(UserToken);
+      managerToken.refreshToken = jest
+        .fn()
+        .mockReturnValueOnce(right("new_token"));
+      diContainer.rebind(UserToken).toConstantValue(managerToken);
+
+      sessionDatabase.sessions = [
+        SessionApplication.create({
+          id: "UUID",
+          userId: "UUID",
+          token: "token",
+          refreshToken: "refreshToken",
+          createdAt: new Date(),
+          active: true,
+          expires: new Date(Date.now() + 1000 * 60 * 60 * 24)
+        })
+      ];
+
+      const token = "token";
+      const response = await supertest
+        .get("/refresh-token")
+        .set("Authorization", `bearer ${token}`);
+
+      expect(response.status).toBe(StatusCodes.OK);
+      expect(response.body).toEqual({ token: "new_token" });
     });
   });
 });
